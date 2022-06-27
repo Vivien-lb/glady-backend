2b24da79a77d4981f6d98df25e470b3b5ac39932
Vivien-lb <vivien-lb@hotmail.fr>
2022-06-27 15:43:51 +0200

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO GLADY_USER (id, name) VALUES (1, 'Vivien');
-INSERT INTO GLADY_USER (id, name) VALUES (2, 'John');   
-
-INSERT INTO COMPANY (id, name, balance) VALUES (1, 'Tesla', 300);
-INSERT INTO COMPANY (id, name, balance) VALUES (2, 'Google', 550);    
